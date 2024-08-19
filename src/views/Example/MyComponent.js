c7f46d6b190660cca7e7bafdb8483590aa9974c4
Vuongvlq <vuong.vlq.1998@gmail.com>
2024-08-19 19:27:41 +0700

    Use React State

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,11 +1,37 @@
 import React from "react";
 
+/**
+ * JSX => Return 1 block code HTML
+ * Fragment => Return multiple block code HTML, code HTML must be wrapped in <Fragment> or <>
+ * State help update data in component but not refresh page
+ */
+
 class MyComponent extends React.Component {
+
+  state = {
+    name: "Vuong",
+    old: 25
+  }
+
+  handleOnChangeName = (event) => {
+    this.setState({
+      name: event.target.value
+    });
+  }
+
   render() {
     return (
-      <div>
-        Hello world my component
-      </div>
+      <>
+        <div>
+          <input type="text" value={this.state.name} onChange={(event) => this.handleOnChangeName(event)} />
+          {console.log("MyComponent render")}
+          Hello my component, I'm {this.state.name}.
+        </div>
+        <div>
+          I'm {this.state["old"]} years old
+        </div>
+      </>
+
     );
   }
 }
