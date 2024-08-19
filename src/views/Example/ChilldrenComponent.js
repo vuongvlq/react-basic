2fb90884354529941a3f3020bd9612d989ef1a90
Vuongvlq <vuong.vlq.1998@gmail.com>
2024-08-20 00:26:15 +0700

    Conditional Outputs

--- src/views/Example/ChilldrenComponent.js
+++ src/views/Example/ChilldrenComponent.js
@@ -7,29 +7,68 @@ import React from "react";
  */
 
 class ChildrenComponent extends React.Component {
+  state = {
+    showJobs: false
+  }
 
+  handleShowHide = () => {
+    this.setState({
+      showJobs: !this.state.showJobs
+    })
+  }
   render() {
-    console.log(">>> check props ", this.props)
-    // let name = this.props.name;
-    // let age = this.props.age;
-
-    let { name, age, address, arrJobs } = this.props;
+    let { arrJobs } = this.props;
+    let { showJobs } = this.state;
     return (
       <>
-        <div className='job-list'>
-          {
-            arrJobs.map((item, index) => {
-              return (
-                <div key={item.id}>
-                  Title: {item.title} - Salary: {item.salary}
-                </div>
-              )
-            })
-          }
-        </div>
+        {showJobs === false ?
+          <div>
+            <button onClick={() => this.handleShowHide()}>Show</button>
+          </div>
+          :
+          <>
+            <div className='job-list'>
+              {
+                arrJobs.map((item, index) => {
+                  return (
+                    <div key={item.id}>
+                      Title: {item.title} - Salary: {item.salary}
+                    </div>
+                  )
+                })
+              }
+            </div>
+            <div>
+              <button onClick={() => this.handleShowHide()} > Hide</button>
+            </div>
+          </>
+        }
       </>
     );
   }
 }
+
+// const ChildrenComponent = (props) => {
+//   console.log(">>> check props ", props)
+//   // let name = this.props.name;
+//   // let age = this.props.age;
+
+//   let { name, age, address, arrJobs } = props;
+//   return (
+//     <>
+//       <div className='job-list'>
+//         {
+//           arrJobs.map((item, index) => {
+//             return (
+//               <div key={item.id}>
+//                 Title: {item.title} - Salary: {item.salary}
+//               </div>
+//             )
+//           })
+//         }
+//       </div>
+//     </>
+//   );
+// }
 
 export default ChildrenComponent;
