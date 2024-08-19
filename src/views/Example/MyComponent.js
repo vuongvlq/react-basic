2177fad52ea7efc2c17cbbb15441208a472337f5
Vuongvlq <vuong.vlq.1998@gmail.com>
2024-08-19 19:05:50 +0700

    Create First Component

---
+++ src/views/Example/MyComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello world my component
+      </div>
+    );
+  }
+}
+
+export default MyComponent;
