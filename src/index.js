5ac74c6229d9658d3ae1c095ff2bbef18d0031c1
Vuongvlq <vuong.vlq.1998@gmail.com>
2024-10-06 21:13:59 +0700

    Setup Redux into React Project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './views/App';
-import reportWebVitals from './reportWebVitals';
-import './styles/global.scss';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './views/App'
+import reportWebVitals from './reportWebVitals'
+import './styles/global.scss'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './store/reducers/rootReducer'
 
+const reduxStore = createStore(rootReducer)
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={reduxStore}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +20,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,14 @@
+const initState = {
+  users: [
+    { id: 1, name: 'user 1' },
+    { id: 2, name: 'user 2' },
+    { id: 3, name: 'user 3' }
+  ]
+}
+
+const rootReducer = (state = initState, action) => {
+
+  return state
+}
+
+export default rootReducer
